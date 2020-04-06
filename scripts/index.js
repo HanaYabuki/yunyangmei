40532733761e7e6bed37489ed40604eebd2ee9ab
HanaYabuki <1847196775@qq.com>
2020-04-06 16:12:33 +0800

    程序基本成型

---
+++ scripts/index.js
@@ -0,0 +1,5 @@
+function closeAd(id) {
+    var tmp = document.getElementById(id)
+    tmp.style.display = "none"
+
+}
---
+++ scripts/ylm.js
@@ -0,0 +1,34 @@
+function talkRight(idTo,txt) {
+    tmp = '<div class="dia-right dia">' 
+        + '<pre>'
+        + txt
+        + '</pre>'
+        + '</div>'
+    document.getElementById(idTo).innerHTML += tmp;
+    window.scrollBy(0,10000000);
+}
+function talkLeft(idTo,txt) {
+    tmp = '<div class="dia-left dia">' 
+        + '<pre>'
+        + txt
+        + '</pre>'
+        + '</div>'
+    document.getElementById(idTo).innerHTML += tmp;
+    window.scrollBy(0,10000000);
+}
+
+function talk(idFrom) {
+    tmp = document.getElementById(idFrom);
+    txt = tmp.value;
+    tmp.value = ""
+    talkRight("talkBox",txt);
+    
+    setTimeout(
+        'talkLeft("talkBox","喵？")',
+        Math.ceil(Math.random()*500) + 500
+    )
+}
+
+function help() {
+    alert("众所周知，人类的语言过于低级，喵星人不屑于使用");
+}
